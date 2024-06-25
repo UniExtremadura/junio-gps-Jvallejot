1de4fbf37e3cffaf468d30e4e2279f58c1d478c1
jvallejot <jvallejot@alumnos.unex.es>
2024-06-25 17:39:58 +0200

    HU-01 implement

--- app/src/main/java/com/example/firstapplication/MainActivity.kt
+++ app/src/main/java/com/example/firstapplication/MainActivity.kt
@@ -43,6 +43,7 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
+    // SE IMPLEMENTA AÑADIR VUELO
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
